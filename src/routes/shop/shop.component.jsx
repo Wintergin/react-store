79a7ed23df51c7ee81f28a04520ad83a0a275713
Jason van Rensburg <wintajin@gmail.com>
2025-06-03 11:01:17 +0200

    Changes to code

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,6 +1,20 @@
+import { useContext } from 'react';
+
+import { ProductsContext } from '../../contexts/products.context';
+import ProductCard from '../../components/product-card/product-card.component';
+
+import './shop.styles.scss';
+
 const Shop = () => {
+  const { products } = useContext(ProductsContext);
+
   return (
-    <p>Hello Shop</p>
+    <div className='products-container'>
+      {
+        products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+    </div>
   );
 };
 
