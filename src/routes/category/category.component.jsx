1ed1374c3dd2b322fd669ef78863e3fe343a861b
Jason van Rensburg <wintajin@gmail.com>
2025-06-05 10:51:21 +0200

    Updates

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,10 +1,11 @@
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { CategoriesContext } from '../../contexts/categories.context';
 
 import './category.styles.scss';
 import ProductCard from '../../components/product-card/product-card.component';
+
 
 const Category = () => {
     const { category } = useParams();
@@ -16,11 +17,14 @@ const Category = () => {
     }, [category, categoriesMap])
 
     return (
-        <div className='category-container'>
-            {products &&
-               products.map((product) => (<ProductCard key={product.id} product={product} />))
-            }
-        </div>
+        <Fragment>
+            <h2 className='title'>{category.toUpperCase()}</h2>
+            <div className='category-container'>
+                {products &&
+                products.map((product) => (<ProductCard key={product.id} product={product} />))
+                }
+            </div>
+        </Fragment>
     )
 };
 
